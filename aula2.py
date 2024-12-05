188c7848d3d7586d87a143255e5aec65d6e3ff30
andrewestscott <vitorandre12345678@gmail.com>
2024-12-05 11:27:00 -0300

    aula 2

---
+++ aula2.py
@@ -0,0 +1,6 @@
+print ('Olá kanye')
+nome_usuario = 'Kanye West' # tipo -> str = string
+idade_usuario = 47 # tipo -> int = integer
+altura_usuario = 1.73 # tipo -> float = flutuante
+maior_idade = True # tipo -> bool = boolean
+print ('Seja bem-vindo', nome_usuario, 'sua idade é', idade_usuario, 'anos', 'você tem', altura_usuario, 'metros. Você é maior de idade?', maior_idade)
